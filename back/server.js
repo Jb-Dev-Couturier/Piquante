e443afc2080e4dd09a6ecabeeaefbfc03070feb6
JB-Dev <jibaoke@gmail.com>
2022-08-30 19:07:47 +0200

    installation dotenv + configuration port

--- back/server.js
+++ back/server.js
@@ -1,9 +1,14 @@
 //Import 
 import express from 'express'
+import dotenv from 'dotenv';
 
 
 //Variable
 const app = express()
+dotenv.config();
+const PORT = process.env.PORT;
 
 //connection serveur
-app.listen(3000, ()=> console.log('Serveur demarée'))
+app.listen(PORT, () =>
+  console.log(`Serveur demarée sur http//http://localhost:${PORT}/`)
+);
