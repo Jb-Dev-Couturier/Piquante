88e9c6909b6964613a76b2fb67e8c1648a0b3f8c
JB-Dev <jibaoke@gmail.com>
2022-08-30 21:36:59 +0200

    installation cores + normalization port et server adress

--- back/server.js
+++ back/server.js
@@ -1,9 +1,11 @@
 //Import
+import http from 'http'
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js'
+import cors from 'cors'
 
 
 //Variable
@@ -13,23 +15,74 @@ const app = express();
 //middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true })); //limite les données par requete
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors()); 
 dotenv.config();
+
+
+//normalisation du port pour requete header
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
+});
+
+
+
+
 
 
 
 //Connection DataBase et serveur
 const DBurl = process.env.MONGO_DB_URL;
-const PORT = process.env.PORT;
 mongoose
   .connect(DBurl, { useNewUrlParser: true, useUnifiedTopology: true }) // Parametre de connexion recommandation mongoDB
 
   .then(() =>
-    app.listen(PORT, () =>
+    server.listen(port, () =>
       console.log(
-        `Connection MongoDB etablie / / Serveur demarée sur http://localhost:${PORT}/` //serveur demarée correctement
+        `Connection MongoDB etablie / / Serveur demarée sur http://localhost:${port}/` //serveur demarée correctement
       )
     )
-  ).catch((error)=>console.log(`Probleme de connexion`+' : '+error)) //catch de l'erreur en cas de probleme
+  )
+  .catch((error) => console.log(`Probleme de connexion` + ' : ' + error)); //catch de l'erreur en cas de probleme
 
   //Creation des routes utile
 
