fc9c49aad0276590fab99b0a3ba29804f553a712
JB-Dev <jibaoke@gmail.com>
2022-08-30 22:30:06 +0200

    login controller + import token (jsonwrbtoken) + init au login

--- back/controllers/UserController.js
+++ back/controllers/UserController.js
@@ -1,7 +1,7 @@
 import UserModel from '../models/userModel.js';
 import bcrypt from 'bcrypt';
 import CryptoJS from 'crypto-js';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -34,6 +34,36 @@ export const registerUser = async (req, res) => {
   }
 };
 
-export const login = async (req,res)=>{
+//connection avec un compte existant
+export const login = async (req, res) => {
+  const password = req.body.password;
+  //appel du hachage du mail pour comparaison existant en DB
+  const hashedEmail = CryptoJS.HmacSHA512(
+    req.body.email,
+    process.env.SECRET_CRYPTOJS_TOKEN
+  ).toString(CryptoJS.enc.Base64);
+  try {
+    const user = await UserModel.findOne({ email: hashedEmail });
 
-}
+    if (user) {
+      //appel de bcrypt pour comparer le password utilisateur enregistre avec celui de la requete
+      const validity = await bcrypt.compare(password, user.password);
+
+      validity
+        ? res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, process.env.JWT_KEY, {
+              expiresIn: '24h',
+            }),
+          })
+        : res.status(400).json('Mot de passe incorrect !');
+    } else {
+      res.status(404).json(`Cette utilisateur n'existe pas`);
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: `Erreur d'enregistrements` + error.message });
+  }
+};
--- back/routes/userRoute.js
+++ back/routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import  {passwordCheck } from '../middleware/passwordCheck.js'
 import  {emailCheck}  from '../middleware/emailCheck.js'
-import { registerUser } from '../controllers/UserController.js';
+import { login, registerUser } from '../controllers/UserController.js';
 
 const router = express.Router()
 
 
 router.post('/signup', emailCheck, passwordCheck, registerUser);
+router.post('/login', login);
 
 export default router
