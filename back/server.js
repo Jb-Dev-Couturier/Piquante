ac5e7a5b5e9c1deb61d510b3d3b352e8e5db36a1
JB-Dev <jibaoke@gmail.com>
2022-08-30 19:50:11 +0200

    connection base de donnée mongo init

--- back/server.js
+++ back/server.js
@@ -1,16 +1,27 @@
-//Import 
-import express from 'express'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
+//Import
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-
 //Variable
-const app = express()
+const app = express();
 dotenv.config();
-const PORT = process.env.PORT;
 
-//connection serveur
-app.listen(PORT, () =>
-  console.log(`Serveur demarée sur http://localhost:${PORT}/`)
-);
+//middleware
+app.use(bodyParser.json({ limit: '30mb', extended: true })); //limite les données par requete
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+
+//Connection DataBase et serveur
+const DBurl = process.env.MONGO_DB_URL;
+const PORT = process.env.PORT;
+mongoose
+  .connect(DBurl, { useNewUrlParser: true, useUnifiedTopology: true }) // Parametre de connexion recommandation mongoDB
+
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Connection MongoDB etablie / / Serveur demarée sur http://localhost:${PORT}/`
+      )
+    )
+  );
