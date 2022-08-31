0cddb9603c4d643e470dddf7f7fe8bb16fc33ee3
JB-Dev <jibaoke@gmail.com>
2022-08-31 12:37:41 +0200

    creation fichier pour sauce (controller, authCheck, inputCheck, model et routes)

---
+++ back/middleware/authCheck.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const authCheck = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+    const userId = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== userId) {
+      throw 'Invalid user ID';
+    } else {
+      next();
+    }
+  } catch {
+    res.status(401).json({
+      error: new Error('Invalid request!'),
+    });
+  }
+};
