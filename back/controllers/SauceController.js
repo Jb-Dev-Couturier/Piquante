527eb4b2376783ed2b9ea93253b5a0680a361c02
JB-Dev <jibaoke@gmail.com>
2022-08-31 20:40:46 +0200

    make it better

--- back/controllers/SauceController.js
+++ back/controllers/SauceController.js
@@ -56,40 +56,134 @@ export const createSauces = async (req, res) => {
 };
 
 //update sauce (+image)
-export const updateSauce = async (req,res)=>{
+export const updateSauce = async (req, res) => {
   const sauceObject = req.file
-  //si on veut modifier l'image
-    ? {
-      //alors on update
+    ? //si on veut modifier l'image
+      {
+        //alors on update
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${
           req.file.filename
         }`,
       }
-      //sinon on envoie seulement la requete sans image modifier
-    : { ...req.body };
-    try {
-      const id = req.params.id;
+    : //sinon on envoie seulement la requete sans image modifier
+      { ...req.body };
+  try {
+    const id = req.params.id;
     const sauce = await SauceModel.updateOne(
       { _id: id },
-      { ...sauceObject, _id: id });
-      res.status(200).json({ message: 'Sauce modifiée' + sauce });
+      { ...sauceObject, _id: id }
+    );
+    res.status(200).json({ message: 'Sauce modifiée' + sauce });
   } catch (error) {
     res.status(500).json({ message: error });
   }
-}
-
+};
 
 //Pour Supprimer Une sauce
-export const DeleteSauce = async (req,res) =>{
+export const DeleteSauce = async (req, res) => {
   const id = req.params.id;
 
   try {
-
-    const sauce = await SauceModel.findById(id)
-      await SauceModel.deleteOne(sauce)
-    res.status(200).json({ message: 'Sauce supprimée'});
+    const sauce = await SauceModel.findById(id);
+    await SauceModel.deleteOne(sauce);
+    res.status(200).json({ message: 'Sauce supprimée' });
   } catch (error) {
     res.status(500).json({ message: error });
   }
-}
+};
+
+//
+export const likeDislikeSauce = async (req, res, next) => {
+  const like = req.body.like;
+
+  //AJOUTER UN LIKE OU UN DISLIKE
+
+  if (like === 1) {
+    try {
+      const sauce = await SauceModel.findOne({ _id: req.params.id });
+      if (
+        sauce.usersDisliked.includes(req.body.userId) ||
+        sauce.usersLiked.includes(req.body.userId)
+      ) {
+        res.status(401).json({ message: 'Opération non autorisée !' });
+      } else {
+        await SauceModel.updateOne(
+          { _id: req.params.id },
+          {
+            //Insère le userId dans le tableau usersLiked du modèle
+            $push: { usersLiked: req.body.userId },
+            //Ajoute le like
+            $inc: { likes: +1 },
+          }
+        );
+      }
+      res.status(200).json({ message: "J'aime !" });
+    } catch (error) {
+      res.status(404).json({ error });
+    }
+  }
+
+  if (like === -1) {
+    try {
+      const sauce = await SauceModel.findOne({ _id: req.params.id });
+      if (
+        sauce.usersDisliked.includes(req.body.userId) ||
+        sauce.usersLiked.includes(req.body.userId)
+      ) {
+        res.status(401).json({ message: 'Opération non autorisée !' });
+      } else {
+        await SauceModel.updateOne(
+          { _id: req.params.id },
+          {
+            //Insère le userId dans le tableau usersLiked du modèle
+            $push: { usersDisliked: req.body.userId },
+            //Ajoute le dislike
+            $inc: { dislikes: +1 },
+          }
+        );
+      }
+      res.status(200).json({ message: "J'aime !" });
+    } catch (error) {
+      res.status(404).json({ error });
+    }
+  }
+
+  //RETIRER SON LIKE OU SON DISLIKE
+
+  if (like === 0) {
+    SauceModel.findOne({ _id: req.params.id })
+      .then((sauce) => {
+        //Regarde si le userId est déjà dans le tableau usersliked/disliked
+        if (sauce.usersLiked.includes(req.body.userId)) {
+          SauceModel.updateOne(
+            { _id: req.params.id },
+            {
+              //Retire le userId dans le tableau usersliked du modèle
+              $pull: { usersLiked: req.body.userId },
+              //Retire le likes
+              $inc: { likes: -1 },
+            }
+          )
+            .then(() => res.status(200).json({ message: "J'aime retiré !" }))
+            .catch((error) => res.status(400).json({ error }));
+        }
+        if (sauce.usersDisliked.includes(req.body.userId)) {
+          SauceModel.updateOne(
+            { _id: req.params.id },
+            {
+              //Retire le userId dans le tableau usersDisliked du modèle
+              $pull: { usersDisliked: req.body.userId },
+              //Retire le dislikes
+              $inc: { dislikes: -1 },
+            }
+          )
+            .then(() =>
+              res.status(200).json({ message: "Je n'aime pas retiré !" })
+            )
+            .catch((error) => res.status(400).json({ error }));
+        }
+      })
+      .catch((error) => res.status(404).json({ error }));
+  }
+};
--- back/routes/sauceRoute.js
+++ back/routes/sauceRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createSauces, DeleteSauce, getAllSauces, getSauce, updateSauce } from '../controllers/SauceController.js';
+import { createSauces, DeleteSauce, getAllSauces, getSauce, likeDislikeSauce, updateSauce } from '../controllers/SauceController.js';
 import { authCheck } from '../middleware/authCheck.js';
 import multer from '../middleware/multer.js';
 import sauceInputCheck from '../middleware/sauceInputCheck.js';
@@ -11,5 +11,6 @@ router.get('/:id', authCheck,getSauce);
 router.post('/', authCheck,multer,sauceInputCheck, createSauces);
 router.put('/:id', authCheck, multer, sauceInputCheck, updateSauce);
 router.delete('/:id', authCheck, DeleteSauce);
+router.post('/:id/like', authCheck, likeDislikeSauce);
 
 export default router;
